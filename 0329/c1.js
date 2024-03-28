86879d7c197d1b067052af02d25fd44fb51c148e
jaypatel0412 <jaypatel.apple01@ou.edu>
2024-03-28 14:10:05 -0500

    lcp 0329

---
+++ 0329/c1.js
@@ -0,0 +1,43 @@
+"use script";
+let R = null;
+
+let b1 = document.querySelector("#b1");
+
+let chart1 = null;
+async function load()
+{
+    let url = `https://juxinglong.github.io/static/data/states.json`;
+
+    let r = await fetch(url);
+    let rj = await r.json();
+
+    let c1 = document.querySelector("#c1");
+
+    //if (chart1 != null)
+    //{
+    //    chart1.destroy();
+    //}
+    
+
+    c1.innerHTML = ``;
+
+    let opts =
+    {
+        type: "pie",
+        data: {
+            labels: rj.map(x => x.st),
+            datasets: [
+                { data: rj.map(x=>x.p), },
+            ],
+        },
+    },
+
+
+    chart1 = new Chart(c1, opts);
+    R = rj;
+    console.log(rj);
+    Swal.fire("Load Data ");
+}
+
+
+b1.addEventListener("click", load);
